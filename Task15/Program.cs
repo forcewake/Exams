ace7135a5570b114910215eb79a8a630ff3a6ee5
spzSource <www.spz@mail.ru>
2013-05-28 21:25:02 +0200

    Task 15 (ArrayHelper)

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -6,17 +6,61 @@ using System.Threading.Tasks;
 
 namespace Task15
 {
-    /*
-     Создать на языке C# статический метод Sum статического класса ArrayHelper, который: 
-- является unsafe-методом;
-- суммирует все элементы целочисленного массива без проверки на выход за границы диапазона массива;
-- в случае возникновения переполнения создает исключение класса OverflowException.
-Реализовать простейший пример использования статического метода Sum на языке C#.
-     */
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	/*
+	 Создать на языке C# статический метод Sum статического класса ArrayHelper, который: 
+ - является unsafe-методом;
+ - суммирует все элементы целочисленного массива без проверки на выход за границы диапазона массива;
+ - в случае возникновения переполнения создает исключение класса OverflowException.
+ Реализовать простейший пример использования статического метода Sum на языке C#.
+	 */
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int sum = 0;
+			int[] arr = new int[1000000];
+			InitArray(arr);
+
+			try
+			{
+				sum = ArrayHelper.Sum(arr);
+			}
+			catch (OverflowException exc)
+			{
+				Console.WriteLine(exc.Message);
+			}
+
+			Console.WriteLine(sum);
+			Console.ReadKey();
+		}
+
+		public static void InitArray(int[] arr)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = i;
+			}
+		}
+	}
+
+	public static class ArrayHelper
+	{
+		public unsafe static int Sum(int[] array)
+		{
+			int sum = 0;
+
+			fixed (int* arrayPointer = array)
+			{
+				for (int i = 0; i < array.Length; i++)
+				{
+					checked
+					{
+						sum += *(arrayPointer + i);
+					}
+				}
+			}
+
+			return sum;
+		}
+	}
 }
